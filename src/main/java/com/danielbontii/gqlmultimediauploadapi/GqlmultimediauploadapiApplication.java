25567e268a2b09cc605b853f9ad0bf08bae8f667
danielbontii <bontiidaniel97@gmail.com>
2023-12-15 10:25:37 +0000

    chore: add project starter files

---
+++ src/main/java/com/danielbontii/gqlmultimediauploadapi/GqlmultimediauploadapiApplication.java
@@ -0,0 +1,13 @@
+package com.danielbontii.gqlmultimediauploadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GqlmultimediauploadapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GqlmultimediauploadapiApplication.class, args);
+	}
+
+}
